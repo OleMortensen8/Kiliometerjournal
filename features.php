943b138a717ff3b24ac46cebb9591ddd13748176
Ole Vølund Skov Mortensen <olemortensen8@yahoo.dk>
2018-08-06 14:28:09 +0200

    Print functionality should be finished (#3)
    * Added printer functionallity via js
    
    * Added .svg
    
    * Fixed Issue  causing the print not to print automagicly

--- features.php
+++ features.php
@@ -9,15 +9,27 @@
                 </div>
                 <div>
                     <div class="table-responsive">
-                        <table class="table">
-                  <?php $table->createTable($db->getDataToSql());
-                   if($db->getDataToSql()){
-                    echo $table->getTable();
-                    }else{
-                    echo '<p style="text-align:center;">Data Kommer Snart</p>';
-                    }
-                    ?>
-                        </table>
+                        <div id="printableArea">
+                                <table class="table">
+                                    <?php $table->createTable($db->getDataToSql());
+                                    if($db->getDataToSql()){
+                                    echo $table->getTable();
+                                }else{
+                                echo '<p style="text-align:center;">Data Kommer Snart</p>';}
+                                ?>
+                                </table>
+                        </div>
+                        <?php if($db->getDataToSql()){ ?>
+    <button class="btn btn-primary" type="button" onclick="printDiv('printableArea')" ><img src="printer.svg"/></button>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/simpleicon" title="Printer">Printer</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    <script>
+    function printDiv(divName) {
+     var printContents = document.getElementById(divName).innerHTML;
+     var originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+     window.print();
+    }</script><?php } ?>
+                        
                     </div>
                 </div>
             </div>
