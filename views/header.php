49f97b8dfc85971b0ef6d0a1e4296105c47408f4
Ole Vølund Skov Mortensen <9146578-olemortensen8@users.noreply.gitlab.com>
2024-08-15 00:21:24 +0200

    Add last stop km value and Chart.js
    Integrated the last stop km value in the Km Start input field and made it readonly if value is present. Added Chart.js script to header and included a new stats page link in the navigation.

--- views/header.php
+++ views/header.php
@@ -8,18 +8,19 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
     <link rel="stylesheet" href="assets/css/main.css">
 <title></title>
-
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
 <div id="wrap">
     <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-        <div class="container"><a class="navbar-brand logo" href="#">Kiliometer Journalen i PHP</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand logo" href="/">Kiliometer Journalen i PHP</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div
                 class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php">Kiliometer instastninger</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="/">Kiliometer instastninger</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="features.php">Kilometer liste</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="stats.php">Statisik</a></li>
                 </ul>
         </div>
         </div>
