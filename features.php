4299c667387bd55c6a224de6eeca0d47759f06a5
Ole Vølund Skov Mortensen <9146578-olemortensen8@users.noreply.gitlab.com>
2024-10-04 08:09:57 +0200

    Update table headers, DB path, and link in features.php
    Added a new table column for "Fuel Remaining" and adjusted table headers formatting. Corrected the SQLite database path in DB.php. Updated the features.php link to point to the correct local file instead of an external URL.
    
    Signed-off-by: Ole Vølund Skov Mortensen <9146578-olemortensen8@users.noreply.gitlab.com>

--- features.php
+++ features.php
@@ -28,7 +28,8 @@ $data = $db->getDataToSql($limit, $offset);
         <section class="clean-block features">
             <div class="container">
                 <div class="block-heading">
-                    <h2 class="text-info" style="letter-spacing:0px;"><a href="https://bootstrapstudio.io/app/features.html"><strong>KILOMETER LISTE</strong></a><br></h2>
+                    <h2 class="text-info" style="letter-spacing:0px;"><a href="features.php"><strong>KILOMETER
+                                LISTE</strong></a><br></h2>
                     <p>Her er et overblik over alle instastede kørseler</p>
                 </div>
                 <div>
